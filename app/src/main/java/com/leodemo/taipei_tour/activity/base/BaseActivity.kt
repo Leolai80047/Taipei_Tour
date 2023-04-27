f4d9b127463b2ecdf218d85b4c59c10d8ccb3402
Leolai80047 <leolai80047@gmail.com>
2023-04-27 21:30:45 +0800

    修正 base activity 錯誤引用 onCreate

--- app/src/main/java/com/leodemo/taipei_tour/activity/base/BaseActivity.kt
+++ app/src/main/java/com/leodemo/taipei_tour/activity/base/BaseActivity.kt
@@ -1,21 +1,20 @@
 package com.leodemo.taipei_tour.activity.base
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
-import androidx.viewbinding.ViewBinding
 import com.leodemo.taipei_tour.di.HiltBaseActivity
 
-abstract class BaseActivity<T: ViewBinding, V: ViewModel>: HiltBaseActivity() {
+abstract class BaseActivity<T : ViewDataBinding, V : ViewModel> : HiltBaseActivity() {
     var binding: T? = null
         private set
 
     abstract val viewModel: V
     abstract val layoutId: Int
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutId)
     }
 }
